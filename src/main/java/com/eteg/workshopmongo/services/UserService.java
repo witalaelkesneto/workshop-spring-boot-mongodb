0719cff7ebbde478c335ccbee88198a5c68cc7c9
witalaelkesneto <100223334+witalaelkesneto@users.noreply.github.com>
2022-10-19 15:13:24 -0300

    Inserção de usuário com POST

--- src/main/java/com/eteg/workshopmongo/services/UserService.java
+++ src/main/java/com/eteg/workshopmongo/services/UserService.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.eteg.workshopmongo.domain.User;
+import com.eteg.workshopmongo.dto.UserDTO;
 import com.eteg.workshopmongo.repository.UserRepository;
 import com.eteg.workshopmongo.services.exception.ObjectNotFoundException;
 
@@ -24,5 +25,12 @@ public class UserService {
 		Optional<User> user = repo.findById(id);
 		return user.orElseThrow(() -> new ObjectNotFoundException("Usuário não encontrado"));
 	}
-
+	
+	public User insert(User obj) {
+		return repo.insert(obj);
+	}
+	
+	public User fromDTO(UserDTO objDTO) {
+		return new User(objDTO.getId() ,objDTO.getName(), objDTO.getEmail());
+	}
 }
