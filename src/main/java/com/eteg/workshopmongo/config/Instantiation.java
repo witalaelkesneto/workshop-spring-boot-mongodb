7db8856c00d011eab2e1940996062e6b17b65432
witalaelkesneto <100223334+witalaelkesneto@users.noreply.github.com>
2022-10-19 20:24:05 -0300

    Acrescentando comentários aos posts

--- src/main/java/com/eteg/workshopmongo/config/Instantiation.java
+++ src/main/java/com/eteg/workshopmongo/config/Instantiation.java
@@ -11,6 +11,7 @@ import org.springframework.context.annotation.Configuration;
 import com.eteg.workshopmongo.domain.Post;
 import com.eteg.workshopmongo.domain.User;
 import com.eteg.workshopmongo.dto.AuthorDTO;
+import com.eteg.workshopmongo.dto.CommentDTO;
 import com.eteg.workshopmongo.repository.PostRepository;
 import com.eteg.workshopmongo.repository.UserRepository;
 
@@ -40,8 +41,15 @@ public class Instantiation implements CommandLineRunner{
 		userRepository.saveAll(Arrays.asList(maria, alex, bob));
 		
 		Post post1 = new Post(null, sdf.parse("21/03/2018"), "Partiu viagem", "Vou viajar para São Paulo. Abraços!", new AuthorDTO(maria));
-		Post post2 = new Post(null, sdf.parse("23/03/2018"), "Bom dia", "Acordei feliz hoje!", new AuthorDTO(maria));  
-
+		Post post2 = new Post(null, sdf.parse("23/03/2018"), "Bom dia", "Acordei feliz hoje!", new AuthorDTO(maria));
+		
+		CommentDTO c1 = new CommentDTO("Boa viagem mano!", sdf.parse("21/03/2018"), new AuthorDTO(alex));
+		CommentDTO c2 = new CommentDTO("Aproveite", sdf.parse("22/03/2018"), new AuthorDTO(bob));
+		CommentDTO c3 = new CommentDTO("Tenha um ótimo dia!", sdf.parse("23/03/2018"), new AuthorDTO(alex));
+		
+		post1.getComments().addAll(Arrays.asList(c1, c2));
+		post2.getComments().add(c3);
+		
 		postRepository.saveAll(Arrays.asList(post1, post2));
 		
 		maria.getPosts().addAll(Arrays.asList(post1, post2));
