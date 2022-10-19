ef1f7ed800c020343027ba8b75dc744a6914cd60
witalaelkesneto <100223334+witalaelkesneto@users.noreply.github.com>
2022-10-19 14:58:35 -0300

    Obtendo um usuário por id

--- src/main/java/com/eteg/workshopmongo/services/UserService.java
+++ src/main/java/com/eteg/workshopmongo/services/UserService.java
@@ -1,12 +1,14 @@
 package com.eteg.workshopmongo.services;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.eteg.workshopmongo.domain.User;
 import com.eteg.workshopmongo.repository.UserRepository;
+import com.eteg.workshopmongo.services.exception.ObjectNotFoundException;
 
 @Service
 public class UserService {
@@ -16,6 +18,11 @@ public class UserService {
 	
 	public List<User> findAll(){
 		return repo.findAll();
+	}
+	
+	public User findById(String id) {
+		Optional<User> user = repo.findById(id);
+		return user.orElseThrow(() -> new ObjectNotFoundException("Usuário não encontrado"));
 	}
 
 }
