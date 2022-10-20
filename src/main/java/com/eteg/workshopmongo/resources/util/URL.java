2643e96317eea9a0813d509c161dc1ee6efb4162
witalaelkesneto <100223334+witalaelkesneto@users.noreply.github.com>
2022-10-19 22:21:30 -0300

    Consulta com vários critérios

--- src/main/java/com/eteg/workshopmongo/resources/util/URL.java
+++ src/main/java/com/eteg/workshopmongo/resources/util/URL.java
@@ -2,9 +2,13 @@ package com.eteg.workshopmongo.resources.util;
 
 import java.io.UnsupportedEncodingException;
 import java.net.URLDecoder;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.TimeZone;
 
 public class URL {
-	
+
 	public static String decodeParam(String text) {
 		try {
 			return URLDecoder.decode(text, "UTF-8");
@@ -13,4 +17,13 @@ public class URL {
 		}
 	}
 
+	public static Date convertDate(String textDate, Date dafaultValue) {
+		SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd");
+		sdf.setTimeZone(TimeZone.getTimeZone("GMT"));
+		try {
+			return sdf.parse(textDate);
+		} catch (ParseException e) {
+			return dafaultValue;
+		}
+	}
 }
--- src/main/java/com/eteg/workshopmongo/services/PostService.java
+++ src/main/java/com/eteg/workshopmongo/services/PostService.java
@@ -1,5 +1,6 @@
 package com.eteg.workshopmongo.services;
 
+import java.util.Date;
 import java.util.List;
 import java.util.Optional;
 
@@ -23,5 +24,9 @@ public class PostService {
 	
 	public List<Post> findByTitle(String text){
 		return repo.searchTitle(text);
+	}
+	
+	public List<Post> fullSearch(String text, Date minDate, Date maxDate){
+		return repo.fullSearch(text, minDate, maxDate);
 	}
 }
